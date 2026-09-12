02186a8a1466e1df86d1980c3a4c6a14e92b6299
Nitil Patel <nitilchaudhary22@gmail.com>
2026-09-13 00:39:07 +0530

    feat: add sum of two integers

---
+++ 01-Cpp-Basics/sum_of_two_int.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    cout << "Enter two integers : ";
+    cin >> a >>b;
+    sum = a+b;
+    cout << "Sum of two integers are : "<< sum;
+     return 0;
+}
