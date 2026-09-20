9a264e6f580cf820b361d6edcdffaaf575298f56
Nitil Chaudhary <nitilchaudhary22@gmail.com>
2026-09-20 23:28:23 +0530

    feat: solve leetcode 1342

--- 01-Cpp-Basics/leetcode_1342.cpp
+++ 01-Cpp-Basics/leetcode_1342.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int step = 0;
 
+        while(num != 0)
+        {
+            if(num % 2 == 0){
+                num = num / 2;
+            }
+            else{
+                num = num - 1;
+            }
+
+            step = step + 1;
+        }
+
+        return step;
+    }
+};
