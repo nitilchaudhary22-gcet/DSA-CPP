41fb5aea6e25e2ff0de92fd223b33eef93284205
Nitil Chaudhary <nitilchaudhary22@gmail.com>
2026-09-20 23:37:22 +0530

    feat

---
+++ 01-Cpp-Basics/leetcode_202.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     bool isHappy(int n) {
+        int sum ;
+        int rem ;
+        while(n>6){
+            sum = 0;
+            while(n>0){
+                rem = n % 10;
+                sum = sum + rem*rem ;
+                n = n / 10;
+            }
+            n = sum;
+            if(sum == 1 || sum == 7){
+                return true ;
+            }
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+};
